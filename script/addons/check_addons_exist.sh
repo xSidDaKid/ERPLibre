5040a56491ff5d3058b720a65a0e53c736a807ea
Mathieu Benoit <mathben@technolibre.ca>
2021-10-11 14:43:54 -0400

    [FIX] WIP check_addons_exist support multiple module

--- script/addons/check_addons_exist.sh
+++ script/addons/check_addons_exist.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 # TODO check only in config path and not all code
-# TODO missing path of odoo
 FILE_NAME=$1
-COUNT=$(find . -name "${FILE_NAME}"|wc -l)
-if [[ $COUNT -eq 0 ]]; then
-  echo "ERROR cannot find module name '${FILE_NAME}'."
-  exit 1
-elif [[ $COUNT -eq 1 ]]; then
-  exit 0
-else
-  echo "ERROR contains ${COUNT} module name '${FILE_NAME}', will create error at installation."
-  exit 2
-fi
+# Need to split by , when contain multiple file_name
+#FILE_NAME_ARR=($(echo $FILE_NAME | tr "," "\n"))
+IFS=',' read -ra FILE_NAME_ARR <<< "$FILE_NAME"
+
+for i in "${FILE_NAME_ARR[@]}"; do
+  COUNT=$(find ./addons ./odoo -name "${i}"|wc -l)
+  if [[ $COUNT -eq 0 ]]; then
+    echo "ERROR cannot find module name '${i}'."
+    exit 1
+  elif [[ $COUNT -ne 1 ]]; then
+    echo "ERROR contains ${COUNT} module name '${i}', will create error at installation."
+    find . -name "${i}"
+    exit 2
+  fi
+done
+exit 0
