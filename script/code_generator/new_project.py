d5eeba09855c28009246c417669006c777f8e07d
Mathieu Benoit <mathben@technolibre.ca>
2021-12-28 18:42:33 -0500

    WIP

--- script/code_generator/new_project.py
+++ script/code_generator/new_project.py
@@ -4,7 +4,14 @@ import logging
 import os
 import sys
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    format=(
+        "%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d]"
+        " %(message)s"
+    ),
+    datefmt="%Y-%m-%d:%H:%M:%S",
+    level=logging.DEBUG,
+)
 _logger = logging.getLogger(__name__)
 
 # TODO Check if exist
@@ -19,7 +26,7 @@ def get_config():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description="""\
-        Create new project 
+        Create new project for a single module with code generator suite.
 """,
         epilog="""\
 """,
@@ -32,24 +39,148 @@ def get_config():
     )
     parser.add_argument(
         "-m",
-        "--module_name",
+        "--module",
         required=True,
         help="Module name to create",
     )
     parser.add_argument(
+        "--directory_code_generator",
+        help="The directory of the code_generator to use.",
+    )
+    parser.add_argument(
+        "--code_generator_name",
+        help="The name of the code_generator to use.",
+    )
+    parser.add_argument(
+        "--directory_template_name",
+        help="The directory of the template to use.",
+    )
+    parser.add_argument(
+        "--template_name",
+        help="The name of the template to use.",
+    )
+    parser.add_argument(
         "-f",
         "--force",
-        required=True,
         help="Force override directory and module.",
     )
     args = parser.parse_args()
     return args
 
 
+class ProjectManagement:
+    def __init__(
+        self,
+        module_name,
+        module_directory,
+        cg_name="",
+        cg_directory="",
+        template_name="",
+        template_directory="",
+        force=False,
+    ):
+        self.force = force
+        self.msg_error = ""
+
+        self.module_directory = module_directory
+        if not os.path.exists(self.module_directory):
+            self.msg_error = (
+                f"Path directory '{self.module_directory}' not exist."
+            )
+            _logger.error(self.msg_error)
+            return
+
+        self.cg_directory = cg_directory if cg_directory else module_directory
+        if not os.path.exists(self.cg_directory):
+            self.msg_error = (
+                f"Path cg directory '{self.cg_directory}' not exist."
+            )
+            _logger.error(self.msg_error)
+            return
+
+        self.template_directory = (
+            template_directory if template_directory else module_directory
+        )
+        if not os.path.exists(self.template_directory):
+            self.msg_error = (
+                f"Path template directory '{self.template_directory}' not"
+                " exist."
+            )
+            _logger.error(self.msg_error)
+            return
+
+        if not module_name:
+            self.msg_error = "Module name is missing."
+            _logger.error(self.msg_error)
+            return
+
+        # Get module name
+        self.module_name = module_name
+        # Get code_generator name
+        self.cg_name = self._generate_cg_name(default=cg_name)
+        # Get template name
+        self.template_name = self._generate_template_name(
+            default=template_name
+        )
+
+    def _generate_cg_name(self, default=""):
+        if default:
+            return default
+        return f"code_generator_{self.module_name}"
+
+    def _generate_template_name(self, default=""):
+        if default:
+            return default
+        return f"code_generator_template_{self.module_name}"
+
+    @staticmethod
+    def validate_path_ready_to_be_override(path):
+        if not os.path.exists(path):
+            return True
+        # Check if in git
+        pass
+
+    def generate_module(self):
+        module_path = os.path.join(self.module_directory, self.module_name)
+        if not self.force and self.validate_path_ready_to_be_override(
+            module_path
+        ):
+            self.msg_error = f"Cannot generate on module path '{module_path}'"
+            _logger.error(self.msg_error)
+            return False
+
+        cg_path = os.path.join(self.cg_directory, self.cg_name)
+        if not self.force and self.validate_path_ready_to_be_override(cg_path):
+            self.msg_error = f"Cannot generate on cg path '{cg_path}'"
+            _logger.error(self.msg_error)
+            return False
+
+        template_path = os.path.join(
+            self.template_directory, self.template_name
+        )
+        if not self.force and self.validate_path_ready_to_be_override(
+            template_path
+        ):
+            self.msg_error = (
+                f"Cannot generate on template path '{template_path}'"
+            )
+            _logger.error(self.msg_error)
+            return False
+
+
 def main():
     config = get_config()
-    if not os.path.exists(config.directory):
-        _logger.error(f"Path directory {config.directory} not exist.")
+    project = ProjectManagement(
+        config.module,
+        config.directory,
+        cg_name=config.code_generator_name,
+        template_name=config.template_name,
+        force=config.force,
+    )
+    if project.msg_error:
+        return -1
+
+    if project.generate_module():
         return -1
 
     return 0
