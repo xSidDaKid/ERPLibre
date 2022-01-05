8b336c9f9cdcc018558e7cdf8d512e3c449c2240
Mathieu Benoit <mathben@technolibre.ca>
2022-01-05 14:17:10 -0500

    [ADD] open_terminal_code_generator.sh: OCA_server-tools

--- script/open_terminal_code_generator.sh
+++ script/open_terminal_code_generator.sh
@@ -6,6 +6,7 @@ paths=(
 "${working_path}/addons/TechnoLibre_odoo-code-generator"
 "${working_path}/addons/TechnoLibre_odoo-code-generator-template"
 "${working_path}/addons/TechnoLibre_odoo_accorderie"
+"${working_path}/addons/OCA_server-tools"
 
 )
 cmd_before="cd "
@@ -14,7 +15,7 @@ cmd_after=";gnome-terminal --tab -- bash -c 'git status;bash';"
 #cmd_after=";gnome-terminal --tab;"
 
 LONGCMD=""
-for t in ${paths[@]}; do
+for t in "${paths[@]}"; do
   LONGCMD+=${cmd_before}${t}${cmd_after}
 done
 
