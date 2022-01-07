753b821bbc3aa9b24451f4ef8a138db61216be1a
Mathieu Benoit <mathben@technolibre.ca>
2022-01-07 16:31:29 -0500

    [UPD] run_parallel_test: merge stdout and stderr - add time execution - better log

--- script/test/run_parallel_test.py
+++ script/test/run_parallel_test.py
@@ -1,9 +1,11 @@
 #!./.venv/bin/python
 import argparse
 import asyncio
+import datetime
 import logging
 import os
 import sys
+import time
 import uuid
 from typing import Tuple
 
@@ -54,7 +56,7 @@ def check_result(task_list, tpl_result):
     lst_warning = []
 
     for i, result in enumerate(tpl_result):
-        lst_log = result[0].split("\n") + result[1].split("\n")
+        lst_log = result[0].split("\n")
         for log_line in lst_log:
             is_ignore_error = False
             if "error" in log_line.lower():
@@ -75,7 +77,7 @@ def check_result(task_list, tpl_result):
                         break
                 if not is_ignore_warning:
                     lst_warning.append(log_line)
-        if result[2]:
+        if result[1]:
             lst_error.append(
                 f"Return status error for test {task_list[i].cr_code.co_name}"
             )
@@ -112,7 +114,7 @@ def print_log(lst_task, tpl_result):
     with open(LOG_FILE, "w") as f:
         for i, task in enumerate(lst_task):
             result = tpl_result[i]
-            status_str = "PASS" if not result[2] else "FAIL"
+            status_str = "PASS" if not result[1] else "FAIL"
             f.write(
                 f"\nTest execution {i + 1} - {status_str} -"
                 f" {task.cr_code.co_name}\n\n"
@@ -120,14 +122,12 @@ def print_log(lst_task, tpl_result):
             if result[0]:
                 f.write(result[0])
                 f.write("\n")
-            if result[1]:
-                f.write(result[1])
-                f.write("\n")
     print(f"Log file {LOG_FILE}")
 
 
 async def run_command(*args):
     # Create subprocess
+    start_time = time.time()
     process = await asyncio.create_subprocess_exec(
         *args,
         # stdout must a pipe to be accessible as process.stdout
@@ -136,10 +136,16 @@ async def run_command(*args):
     )
     # Wait for the subprocess to finish
     stdout, stderr = await process.communicate()
-    # Return stdout, stderr, returncode
-    str_out = stdout.decode().strip() if stdout else ""
-    str_err = stderr.decode().strip() if stderr else ""
-    return str_out, str_err, process.returncode
+    end_time = time.time()
+    diff_sec = end_time - start_time
+    # Return stdout + stderr, returncode
+    str_out = "\n" + stdout.decode().strip() + "\n" if stdout else ""
+    str_err = "\n" + stderr.decode().strip() + "\n" if stderr else ""
+    status_str = "FAIL" if process.returncode else "PASS"
+    str_output_init = (
+        f"\n\n{status_str} [{diff_sec:.3f}s] Execute \"{' '.join(args)}\"\n\n"
+    )
+    return str_output_init + str_out + str_err, process.returncode
 
 
 async def test_exec(
@@ -149,66 +155,53 @@ async def test_exec(
     search_class_module=None,
     script_after_init_check=None,
     lst_init_module_name=None,
-) -> Tuple[str, str, int]:
+) -> Tuple[str, int]:
 
-    res = ""
-    err = ""
-    status = 0
+    test_result = ""
+    test_status = 0
 
+    # Check code, init module to install
     if lst_init_module_name:
         for module_name in lst_init_module_name:
-            res1, err1, status1 = await run_command(
+            res, status = await run_command(
                 "./script/code_generator/check_git_change_code_generator.sh",
                 path_module_check,
                 module_name,
             )
-            res += (
-                "\nTest"
-                " ./script/code_generator/check_git_change_code_generator.sh"
-                f" {path_module_check} {module_name}"
-            )
-            res += "\n" + res1 if res1 else ""
-            err += "\n" + err1 if err1 else ""
-            status += status1
+            test_result += res
+            test_status += status
 
+    # Check code, module to generate
     if module_to_generate:
-        res1, err1, status1 = await run_command(
+        res, status = await run_command(
             "./script/code_generator/check_git_change_code_generator.sh",
             path_module_check,
             module_to_generate,
         )
-        res += (
-            "\nTest"
-            " ./script/code_generator/check_git_change_code_generator.sh"
-            f" {path_module_check} {module_to_generate}"
-        )
-        res += "\n" + res1 if res1 else ""
-        err += "\n" + err1 if err1 else ""
-        status += status1
+        test_result += res
+        test_status += status
 
-    # Leave when detect anomaly
-    if status:
-        return res, err, status
+    # Leave when detect anomaly in check before start
+    if test_status:
+        return test_result, test_status
 
-    if script_after_init_check and not status:
-        res2, err2, status = await run_command(script_after_init_check)
-        res += "\n" + res2 if res2 else ""
-        err += "\n" + err2 if err2 else ""
+    # Execute script before start
+    if script_after_init_check and not test_status:
+        res, status = await run_command(script_after_init_check)
+        test_result += res
+        test_status += status
 
     is_db_create = False
     unique_database_name = f"test_demo_{uuid.uuid4()}"[:63]
-    if not status:
-        res2, err2, status = await run_command(
+    if not test_status:
+        res, status = await run_command(
             "./script/db_restore.py", "--database", unique_database_name
         )
-        res += (
-            f"\nTest ./script/db_restore.py --database {unique_database_name}"
-        )
-        res += "\n" + res2 if res2 else ""
-        err += "\n" + err2 if err2 else ""
+        test_result += res
+        test_status += status
         is_db_create = not status
 
-    if not status and lst_init_module_name:
+    if not test_status and lst_init_module_name:
         # Parallel execution here
 
         # No parallel execution here
@@ -218,16 +211,15 @@ async def test_exec(
             if module_to_generate
             else "./script/addons/install_addons.sh"
         )
-        res_app, err_app, status = await run_command(
+        res, status = await run_command(
             script_name,
             unique_database_name,
             str_test,
         )
-        res += f"\nTest {script_name} {unique_database_name} {str_test}"
-        res += "\n" + res_app if res_app else ""
-        err += "\n" + err_app if err_app else ""
+        test_result += res
+        test_status += status
 
-    if not status and search_class_module and module_to_install:
+    if not test_status and search_class_module and module_to_install:
         path_template_to_generate = os.path.join(
             path_module_check, module_to_generate
         )
@@ -237,7 +229,7 @@ async def test_exec(
         # Parallel execution here
 
         # No parallel execution here
-        res_app, err_app, status = await run_command(
+        res, status = await run_command(
             "./script/code_generator/search_class_model.py",
             "--quiet",
             "-d",
@@ -245,34 +237,25 @@ async def test_exec(
             "-t",
             path_template_to_generate,
         )
-        res += (
-            "\nTest ./script/code_generator/search_class_model.py --quiet -d"
-            f" {path_module_to_generate} -t {path_template_to_generate}"
-        )
-        res += "\n" + res_app if res_app else ""
-        err += "\n" + err_app if err_app else ""
+        test_result += res
+        test_status += status
 
-    if not status and module_to_generate and module_to_install:
+    if not test_status and module_to_generate and module_to_install:
         # Parallel execution here
 
         # No parallel execution here
-        res_app, err_app, status = await run_command(
+        res, status = await run_command(
             "./script/code_generator/install_and_test_code_generator.sh",
             unique_database_name,
             module_to_generate,
             path_module_check,
             module_to_install,
         )
-        res += (
-            "\nTest"
-            " ./script/code_generator/install_and_test_code_generator.py"
-            f" {unique_database_name} {module_to_generate} {path_module_check} {module_to_install}"
-        )
-        res += "\n" + res_app if res_app else ""
-        err += "\n" + err_app if err_app else ""
+        test_result += res
+        test_status += status
 
     if is_db_create:
-        res3, err3, status_db = await run_command(
+        res, status = await run_command(
             "./.venv/bin/python3",
             "./odoo/odoo-bin",
             "db",
@@ -280,15 +263,13 @@ async def test_exec(
             "--database",
             unique_database_name,
         )
-        res += f"\nTest drop database {unique_database_name}"
-        res += "\n" + res3 if res3 else ""
-        err += "\n" + err3 if err3 else ""
-        status = status if status else status_db
+        test_result += res
+        test_status += status
 
-    return res, err, status
+    return test_result, test_status
 
 
-async def run_demo_test() -> Tuple[str, str, int]:
+async def run_demo_test() -> Tuple[str, int]:
     lst_test_name = [
         "demo_helpdesk_data",
         "demo_internal",
@@ -299,20 +280,19 @@ async def run_demo_test() -> Tuple[str, str, int]:
         "demo_website_leaflet",
         "demo_website_snippet",
     ]
-    res, err, status = await test_exec(
+    res, status = await test_exec(
         "./addons/TechnoLibre_odoo-code-generator-template",
         lst_init_module_name=lst_test_name,
     )
 
-    return res, err, status
+    return res, status
 
 
-async def run_mariadb_test() -> Tuple[str, str, int]:
-    res = ""
-    err = ""
-    status = 0
+async def run_mariadb_test() -> Tuple[str, int]:
+    test_result = ""
+    test_status = 0
     # Migrator
-    res1, err1, status1 = await test_exec(
+    res, status = await test_exec(
         "./addons/TechnoLibre_odoo-code-generator-template",
         module_to_install="demo_mariadb_sql_example_1",
         module_to_generate=(
@@ -325,11 +305,11 @@ async def run_mariadb_test() -> Tuple[str, str, int]:
             "code_generator_portal",
         ],
     )
-    res += "\n" + res1 if res1 else ""
-    err += "\n" + err1 if err1 else ""
-    status += status1
+    test_result += res
+    test_status += status
+
     # Template
-    res2, err2, status2 = await test_exec(
+    res, status = await test_exec(
         "./addons/TechnoLibre_odoo-code-generator-template",
         module_to_install="code_generator_demo_mariadb_sql_example_1",
         module_to_generate=(
@@ -341,28 +321,27 @@ async def run_mariadb_test() -> Tuple[str, str, int]:
             "demo_mariadb_sql_example_1",
         ],
     )
-    res += "\n" + res2 if res2 else ""
-    err += "\n" + err2 if err2 else ""
-    status += status2
+    test_result += res
+    test_status += status
+
     # Code generator
-    res3, err3, status3 = await test_exec(
+    res, status = await test_exec(
         "./addons/TechnoLibre_odoo-code-generator-template",
         module_to_install="demo_mariadb_sql_example_1",
         module_to_generate="code_generator_demo_mariadb_sql_example_1",
     )
-    res += "\n" + res3 if res3 else ""
-    err += "\n" + err3 if err3 else ""
-    status += status3
+    test_result += res
+    test_status += status
 
-    return res, err, status
+    return test_result, test_status
 
 
-async def run_helloworld_test() -> Tuple[str, str, int]:
-    res, err, status = await run_command(
+async def run_helloworld_test() -> Tuple[str, int]:
+    res, status = await run_command(
         "./test/code_generator/hello_world.sh",
     )
 
-    return res, err, status
+    return res, status
 
 
 def print_summary_task(task_list):
@@ -373,8 +352,8 @@ def print_summary_task(task_list):
 def run_all_test() -> None:
     task_list = []
 
-    # task_list.append(run_demo_test())
-    # task_list.append(run_helloworld_test())
+    task_list.append(run_demo_test())
+    task_list.append(run_helloworld_test())
     task_list.append(run_mariadb_test())
 
     print_summary_task(task_list)
@@ -389,8 +368,12 @@ def run_all_test() -> None:
 
 def main():
     config = get_config()
-
+    start_time = time.time()
     run_all_test()
+    end_time = time.time()
+    diff_sec = end_time - start_time
+    # print(f"Time execution {diff_sec:.3f}s")
+    print(f"Time execution {datetime.timedelta(seconds=diff_sec)}")
 
     return 0
 
